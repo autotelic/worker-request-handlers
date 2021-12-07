38c3d5c496bd257fa489f210bb6a6c64d7f2d4ee
Edward Stone <ed@autotelic.com>
2021-12-07 12:43:45 -0800

    feat: toggle basic auth with config

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import { createAuthorizationHandler } from './lib/authorizationHandler'
 import { createApiProxyHandler } from './lib/apiProxyHandler'
 import { createCookieEncryptor } from './lib/createCookieEncryptor'
 import {
-  basicAuthenticationHandler,
+  createBasicAuthenticationHandler,
   createKVCredentialsVerifier
 } from './lib/basicAuthenticationHandler'
 
 export {
   createSitesHandler,
   createSitesErrorHandler,
-  basicAuthenticationHandler,
+  createBasicAuthenticationHandler,
   createKVCredentialsVerifier,
   createAuthorizationHandler,
   createApiProxyHandler,
